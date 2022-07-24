2d27d90022a887bf181f3f78178cec1a7387bada
JAN GUILHERME DE AGUIAR JUNIOR <jangaguiar@gmail.com>
2022-07-24 01:13:11 -0300

    Alertas de açoes habilitado

--- dafamilia/__init__.py
+++ dafamilia/__init__.py
@@ -4,12 +4,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_material import Material
 
 app = Flask(__name__)
 
 
-bootstrap = Bootstrap5(app)
+
 # chave criada com o Python secrets - hex(16)
 app.config['SECRET_KEY'] = 'a96187d522ac0db61233c854d5ed9e2b'
 # configurando o local  da criação do DB
@@ -22,6 +21,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+# incorpora o Bootstrap 5
+bootstrap = Bootstrap5(app)
 
 # importar aqui pois os routs precisam do app para funcionar
 # pylint: disable=wrong-import-position
